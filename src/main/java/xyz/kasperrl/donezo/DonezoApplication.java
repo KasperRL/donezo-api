7a7415ed721ccf561a0f2815a6cebeed1922ad91
Kasper Ligthart <kasper@ligthart.dev>
2026-03-04 21:24:27 +0100

    initial commit

---
+++ src/main/java/xyz/kasperrl/donezo/DonezoApplication.java
@@ -0,0 +1,13 @@
+package xyz.kasperrl.donezo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonezoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonezoApplication.class, args);
+    }
+
+}
